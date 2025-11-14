1454385c6c288fc7c5ecc33c08907771b6e37de6
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-11-14 11:30:21 +0000

    Fix more tests

--- tasks/update-fixtures.mjs
+++ tasks/update-fixtures.mjs
@@ -2,11 +2,9 @@ import { globSync } from "glob";
 import fs from "fs";
 import nunjucks from "nunjucks";
 
-// require.extensions[".njk"] = function (module, filename) {
-//   module.exports = fs.readFileSync(filename, "utf8");
-// };
+const fsPromises = fs.promises;
 
-nunjucks.configure(__dirname + "/../src");
+nunjucks.configure("src");
 
 const componentsDirectory = "src/nationalarchives/components/";
 const componentFixturesFile = "/fixtures.json";
@@ -17,48 +15,52 @@ const components = globSync(
     .replace(new RegExp(`^${componentsDirectory}`), "")
     .replace(new RegExp(`${componentFixturesFile}$`), ""),
 );
-components.forEach((component) => {
-  const componentFixtures = require(
-    `../${componentsDirectory}${component}${componentFixturesFile}`,
-  );
-  const componentNunjucks = require(
-    `../${componentsDirectory}${component}/template.njk`,
-  );
-  const newComponentFixtures = {
-    ...componentFixtures,
-    fixtures: componentFixtures.fixtures.map((fixture) => ({
-      ...fixture,
-      html: nunjucks
-        .renderString(componentNunjucks, {
-          params: fixture.options,
-        })
-        .trim()
-        .replace(/>\n\s*/g, ">")
-        .replace(/\n\s*</g, "<"),
-    })),
-  };
+await components.forEach(async (component) => {
+  await Promise.all([
+    import(`../${componentsDirectory}${component}${componentFixturesFile}`, {
+      with: { type: "json" },
+    }),
+    fsPromises.readFile(`${componentsDirectory}${component}/template.njk`, {
+      encoding: "utf8",
+    }),
+  ]).then(([componentFixtures, componentNunjucks]) => {
+    const newComponentFixtures = {
+      ...componentFixtures.default,
+      fixtures: componentFixtures.default.fixtures.map((fixture) => ({
+        ...fixture,
+        html: nunjucks
+          .renderString(componentNunjucks, {
+            params: fixture.options,
+          })
+          .trim()
+          .replace(/>\n\s*/g, ">")
+          .replace(/\n\s*</g, "<"),
+      })),
+    };
 
-  const allFixtureDifferences = newComponentFixtures.fixtures.reduce(
-    (differences, fixture) =>
-      fixture.html !==
-      componentFixtures.fixtures.find((f) => f.name === fixture.name)?.html
-        ? differences + 1
-        : differences,
-    0,
-  );
-
-  if (allFixtureDifferences) {
-    fs.writeFile(
-      `${componentsDirectory}${component}${componentFixturesFile}`,
-      `${JSON.stringify(newComponentFixtures, null, 2).trim()}\n`,
-      (err) => {
-        if (err) throw err;
-        console.log(
-          `${allFixtureDifferences} ${component} fixture(s) updated successfully`,
-        );
-      },
+    const allFixtureDifferences = newComponentFixtures.fixtures.reduce(
+      (differences, fixture) =>
+        fixture.html !==
+        componentFixtures.default.fixtures.find((f) => f.name === fixture.name)
+          ?.html
+          ? differences + 1
+          : differences,
+      0,
     );
-  }
+
+    if (allFixtureDifferences) {
+      fs.writeFile(
+        `${componentsDirectory}${component}${componentFixturesFile}`,
+        `${JSON.stringify(newComponentFixtures, null, 2).trim()}\n`,
+        (err) => {
+          if (err) throw err;
+          console.log(
+            `${allFixtureDifferences} ${component} fixture(s) updated successfully`,
+          );
+        },
+      );
+    }
+  });
 });
 
 // TODO
